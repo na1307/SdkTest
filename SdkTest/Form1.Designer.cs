332a7eb46c0f3116220048e68ed112ddea5d79ec
na1307 <na1307@outlook.kr>
2024-01-26 15:41:49 +0900

    Add

---
+++ SdkTest/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SdkTest {
+    partial class Form1 {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                if (components != null) {
+                    components.Dispose();
+                }
+                if (cancellationTokenSource != null) {
+                    cancellationTokenSource.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "얼마까지 찾을까? :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(328, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 29);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "1800";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 47);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(710, 366);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 419);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(710, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+
+}
---
+++ SdkTest/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SdkTest {
+    public partial class Form1 : Form {
+        private bool isRunning;
+        private CancellationTokenSource cancellationTokenSource;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e) {
+            if (!isRunning) {
+                isRunning = true;
+                button1.Text = "중지";
+                textBox2.Text = "1";
+                cancellationTokenSource = new CancellationTokenSource();
+
+                try {
+                    await addPrime(cancellationTokenSource.Token);
+                } catch (TaskCanceledException) {
+                    // 중지
+                } finally {
+                    isRunning = false;
+                    button1.Text = "시작";
+                    cancellationTokenSource.Dispose();
+                }
+            } else {
+                cancellationTokenSource.Cancel();
+            }
+        }
+
+        private async Task addPrime(CancellationToken token) {
+            if ((!int.TryParse(textBox1.Text, out var asdf)) || asdf < 2) {
+                MessageBox.Show("숫자가 아닌 것을 입력하거나 범위를 벗어남");
+                return;
+            }
+
+            for (int i = 2; i <= asdf; i++) {
+                if (await isPrime(i, token)) {
+                    textBox2.AppendText(", " + i.ToString());
+                }
+            }
+        }
+
+        private static Task<bool> isPrime(int num, CancellationToken token) {
+            return Task.Run(isPrimeInternal, token);
+
+            bool isPrimeInternal() {
+                for (int i = 2; i < num; i++) {
+                    if (num % i == 0) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
